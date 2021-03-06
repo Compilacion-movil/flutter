7b62902a1dbc09dcc10c2a117e84926795759cb5
antocara <antocara@gmail.com>
2021-03-06 17:42:44 +0100

    añadido código showBottomSheet

--- ejemplo_modal_bottom_sheet/lib/home_screen.dart
+++ ejemplo_modal_bottom_sheet/lib/home_screen.dart
@@ -15,8 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Text("Ejemplo Modal Bottom Sheet"),
         ),
-        body: _gridView()
-        );
+        body: _gridView());
   }
 
   void _showModalBottomSheet(BuildContext context) {
@@ -73,9 +72,50 @@ class _HomeScreenState extends State<HomeScreen> {
             onTap: () => _showModalBottomSheet(context),
             child: Card(
               child: Center(
-                  child: Image.network('https://placeimg.com/640/640/nature/$index')),
+                  child: Image.network(
+                      'https://placeimg.com/640/640/nature/$index')),
             ),
           );
         });
+  }
+
+  void _showModalBottomSheetPermanent(BuildContext context) {
+    Scaffold.of(context).showBottomSheet<void>(
+      (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: new Icon(Icons.share),
+              title: new Text('Share'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: new Icon(Icons.link),
+              title: new Text('Get link'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: new Icon(Icons.edit),
+              title: new Text('Edit name'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: new Icon(Icons.delete),
+              title: new Text('Delete Collection'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
